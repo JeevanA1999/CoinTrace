fc8474e0e10d90f755948a87f5494b1dcb30e6c6
JeevanA1999 <jeevukulal96@gmail.com>
2024-07-07 12:13:07 +0530

    new fix

--- src/Pages/Coin/Coin.jsx
+++ src/Pages/Coin/Coin.jsx
@@ -1,73 +1,87 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./Coin.css";
 import { useParams } from "react-router-dom";
-import { useContext } from "react";
 import { CoinContext } from "../../Context/CoinContext";
 import LineChart from "../../Components/LineChart/LineChart";
+
 const Coin = () => {
   const { coinId } = useParams();
   const [coinData, setCoinData] = useState();
   const [historicalData, setHistoricalData] = useState();
+  const [timeRange, setTimeRange] = useState('10');
+
+  const handleTimeRange = (time) => () => {
+    setTimeRange(time);
+  };
 
   const { currency } = useContext(CoinContext);
 
   const fetchCoinData = async () => {
     const options = {
       method: "GET",
-      mode:'cors',
+      mode: 'cors',
       headers: {
         accept: "application/json",
         "x-cg-demo-api-key": "CG-yQuMqVjDJzyxAERViTRDUCqy",
       },
     };
 
-    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
-      .then((response) => response.json())
-      .then((response) => setCoinData(response))
-      .catch((err) => console.error(err));
+    try {
+      const response = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options);
+      const data = await response.json();
+      setCoinData(data);
+    } catch (err) {
+      console.error(err);
+    }
   };
+
   const fetchHistoricalData = async () => {
     const options = {
       method: "GET",
-      mode:'cors',
+      mode: 'cors',
       headers: {
         accept: "application/json",
         "x-cg-demo-api-key": "CG-yQuMqVjDJzyxAERViTRDUCqy",
       },
     };
 
-    fetch(
-      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`,
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => setHistoricalData(response))
-      // .then((response) => console.log(response))
-      .catch((err) => console.error(err));
+    try {
+      const response = await fetch(
+        
+        `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=${timeRange}?interval=hoverly`,
+        options
+      );
+      const data = await response.json();
+      setHistoricalData(data);
+    } catch (err) {
+      console.error(err);
+    }
   };
- 
- 
-useEffect(()=>{
-  fetchCoinData();
-  fetchHistoricalData()
-},[currency])
 
+  useEffect(() => {
+    fetchCoinData();
+    fetchHistoricalData();
+  }, [currency, timeRange]);
 
-
-  if (coinData,historicalData) {
+  if (coinData && historicalData) {
     return (
       <div className="coin">
         <div className="coin-name">
-          <img src={coinData.image.large} alt=""/>
+          <img src={coinData.image.large} alt="" />
           <p>
             <b>
-              {coinData.name}({coinData.symbol.toUpperCase()})
+              {coinData.name} ({coinData.symbol.toUpperCase()})
             </b>
           </p>
         </div>
         <div className="coin-chart">
-          {/* <div>{historicalData}</div> */}
-          <LineChart historicalData={historicalData}/>
+          <LineChart historicalData={historicalData} />
+        </div>
+        <div className="coin-timeRange">
+          <button onClick={handleTimeRange(1)}>24 Hours</button>
+          <button onClick={handleTimeRange(30)}>30 Days</button>
+          <button onClick={handleTimeRange(90)}>3 Months</button>
+          <button onClick={handleTimeRange(365)}>1 Year</button>
         </div>
         <div className="coin-info">
           <ul>
@@ -84,11 +98,11 @@ useEffect(()=>{
           </ul>
           <ul>
             <li>24 Hour High</li>
-            <li>{currency.Symbol} {coinData.market_data.high_24[currency.name].toLocaleString()}</li>
+            <li>{currency.Symbol} {coinData.market_data.high_24h[currency.name].toLocaleString()}</li>
           </ul>
           <ul>
             <li>24 Hour Low</li>
-            <li>{currency.Symbol} {coinData.market_data.low_24[currency.name].toLocaleString()}</li>
+            <li>{currency.Symbol} {coinData.market_data.low_24h[currency.name].toLocaleString()}</li>
           </ul>
         </div>
       </div>
